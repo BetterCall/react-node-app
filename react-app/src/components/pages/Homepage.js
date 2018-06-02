c07da3b840909e9c55a8ca0019453fe367dc296f
DrSaul <bryann@brovia.com>
2018-06-02 19:07:12 +0200

    React Router Setup
    react router setup
    add login and home pages

---
+++ react-app/src/components/pages/Homepage.js
@@ -0,0 +1,14 @@
+/* @flow */
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Homepage</h1>
+        <Link to='/login'>Login</Link>
+      </div>
+    )
+  }
+}
